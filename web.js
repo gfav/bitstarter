7dcc09d65ad1071c4db30c8423fca71baef53e00
Mohammed Sadiq <moha.for.all@gmail.com>
2013-07-23 21:08:20 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  /*response.send('Hello World2!');*/
-var fs=require('fs');
-var k=fs.readFileSync('./index.html','utf8');
+app.get('/', function(req, res) {
+  /*res.send('hello world');*/
+  var fs = require('fs');
+  var k = fs.readFileSync('./index.html','utf8');
+res.send( k );
 });
 
 var port = process.env.PORT || 5000;
