45d6564ab0a679c618a1ae73c68fba5e824cf4d2
GFAV <moha.for.all@gmail.com>
2013-07-29 19:06:03 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
 res.send( k );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
